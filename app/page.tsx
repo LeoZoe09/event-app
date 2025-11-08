737f0b34a94cbd9ff988e057fdf2c048612c3885
Leonie <leonie@example.com>
2025-11-08 16:32:15 +0100

    final

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.PUBLIC_NEXT_BASE_URL;
 
 const Page = async () => {
     'use cache';
